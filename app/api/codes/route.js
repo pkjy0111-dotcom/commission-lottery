95774c0e6e5d1062f56120b72c9fce836978ae92
pkjy0111-dotcom <pkjy0111@gmail.com>
2026-03-26 01:19:21 +0900

    Update route.js

--- app/api/codes/route.js
+++ app/api/codes/route.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
